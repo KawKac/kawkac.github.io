c220fb7f609bcceba3cad880398252f66ce40089
Kacper Kawala <kacper.jerzy.kawala@gmail.com>
2022-09-16 13:04:31 +0200

    v1.0

---
+++ EN.php
@@ -0,0 +1,68 @@
+<div class="body_style">
+  <div class="about_me">
+    <p>
+      <img src="image/zdj.jpg" alt="Moje zdjęcie"><br>
+      I'm starting my adventure with programming. I study IT on Universyty of Silesia. I prefered this language:
+      <br>
+        <img src="./image/java.svg" alt="Java" class="icon-list">
+        <img src="./image/cpp.svg" alt="C++" class="icon-list">
+      <br>
+      I known some laguange to creat WebSites, like:
+      <br>
+        <img src="./image/html5.svg" alt="HTML" class="icon-list">
+        <img src="./image/css3.svg" alt="CSS" class="icon-list">
+        <img src="./image/js.svg" alt="JavaScript" class="icon-list">
+        <img src="./image/php.svg" alt="PHP" class="icon-list">
+      <br>
+      <hr>
+      Programming is not my only single hobby. I read about new technology connected with IT. I tried be up to data with new computer components.
+      Apart from IT I love physics, read about new discovered in space or in quantum state.
+    </p>
+  </div>
+  <div class="my_story">
+    <h1>My projects:</h1><br>
+    <ul>
+      <li><h5>WoTReservClan</h5></li>
+      <p>
+        Aplikacja do puszczania rezerw osobistych w grze World of Tanks. Pomysł powstał po tym, jak odkryłem, że Wargaming udastępnił pełne API, które można wykorzystać. Kod źródłowy znajduje się pod <a href="https://github.com/KawKac/WoTReservClan" class="text-primary">linkiem</a><br>
+        <div class="small-info">Wykorzystane języki: Kotlin, Groovy</div>
+      </p>
+      <li><h5>Aplikacja do nauki Kotlina</h5></li>
+      <p>
+        Projekt, gdzie pod okiem seniora, uczę się programować w języku Kotlin oraz Groovy. Kod źródłowy można przejrzeć pod <a href="https://github.com/lipov1991/ProductInventory" class="text-primary">linkiem</a>. Aplikacja jest wciąż aktualizowana, ze względów rozwijania swoich umiejętności programistycznych.<br>
+        <div class="small-info">Wykorzystane języki: Kotlin, Groovy</div>
+      </p>
+      <li><h5>Ta strona</h5></li>
+      <p>
+        Głównym moim projektem którym się zajmujem jest ta strona aby ją jak najszybciej ukończyć i później się wziąć za ulepszanie jej (aby zobaczyć kod źródłowy wystarczy kliknąć w ten <a href="https://github.com/KawKac/KawKac.git" class="text-primary">link</a>)<br>
+        <div class="small-info">Wykorzystane języki: HTML, CSS, Bootstrap</div>
+      </p>
+      <li><h5>Gra w kości</h5></li>
+      <p>
+        Prosty program, który polega na wylosowaniu cyfry od 1-6. Aby wygrać należy odgadnąć cyfrę. Co zgadywanie losowana jest nowa cyfra. Program zawiera zarówno grę solo jak i również multiplayer. Do sprawdzania działania programu wykorzystano bota (PC vs. PC)(aby zobaczyć kod źródłowy wystarczy kliknąć w ten <a href="https://github.com/KawKac/Dice" class="text-primary">link</a>).<br>
+        <div class="small-info">Wykorzystane języki: Java</div>
+      </p>
+      <li><h5>Strona na obronę inżyniera</h5></li>
+      <p>
+        Jest to moja praca technika, którą broniłem pod koniec mojego technikum. Jest na temat nowych znaczników HTML, które pojawiły się w HTML5.(aby zobaczyć kod źródłowy wystarczy kliknąć w ten <a href="https://github.com/KawKac/praca_technika" class="text-primary">link</a>).<br>
+        <div class="small-info">Wykorzystane języki: HTML, CSS, JavaScript z biblioteką JQuery</div>
+      </p>
+    </ul>
+  </div>
+</div>
+<footer id="footer">
+  <div class="contact_left">
+    <form class="contact_form" action="index.html" method="post">
+      <h1>Contact:</h1><br>
+      <input type="text" name="name" value="" placeholder="Name and surname or name of company"><br>
+      <input type="text" name="contact" value="" placeholder="Phone number or adress e-mail to contact"><br>
+      <textarea name="message" rows="8" cols="80" placeholder="Message"></textarea><br>
+      <input type="submit" name="submit" value="Send" class="btn btn-success" disabled>
+    </form>
+  </div>
+  <div class="contact_right">
+    <p>Kacper Kawala</p>
+    <p>Adress e-mail: <a href="mailto: kacper.jerzy.kawala@gmail.com" class="text-info">kacper.jerzy.kawala@gmail.com</a></p>
+    <p>Phone Number: +48 575-463-884</p>
+  </div>
+</footer>
---
+++ PL.php
@@ -0,0 +1,74 @@
+      <div class="body_style">
+        <div class="about_me">
+          <p>
+            <img src="image/zdj.jpg" alt="Moje zdjęcie"><br>
+            Jestem pooczątkującym programistą. Aktualnie studiuję informatykę na Uniwersytecie Śląskim. Aktualnymi językami jakimi się interesuję to:<br>
+              <img src="./image/java.svg" alt="Java" class="icon-list">
+              <img src="./image/cpp.svg" alt="C++" class="icon-list">
+              <br>
+            Dodatkowo znam jeszcze podstawę języków programowania aplikacji internetowych takich jak:<br>
+              <img src="./image/html5.svg" alt="HTML" class="icon-list">
+              <img src="./image/css3.svg" alt="CSS" class="icon-list">
+              <img src="./image/js.svg" alt="JavaScript" class="icon-list">
+              <img src="./image/php.svg" alt="PHP" class="icon-list">
+              <br>
+            Podczas programowania stron internetowych poznałem dodatkowo <br>
+              <img src="./image/bootstrap.svg" alt="Bootstrap" class="icon-list">
+            <hr>
+            Oprócz programowania interesuję się również sprzętem komputerowym. Staram się być w miarę na bierząco z nowinkami technologicznymi.
+            Dodatkowo cztytam książki na temat fizyki. Moimi ulubionymi działami z fizyki są:
+            <div class="about_me-list">
+              <ul>
+                <li>Fizyka kwantowa</li>
+                <li>Astrofizyka (nie myślić z astrologią)</li>
+              </ul>
+            </div>
+          </p>
+        </div>
+        <div class="my_story">
+          <h1>Projekty:</h1><br>
+          <ul>
+            <li><h5>WoTReservClan</h5></li>
+            <p>
+              Aplikacja do puszczania rezerw osobistych w grze World of Tanks. Pomysł powstał po tym, jak odkryłem, że Wargaming udastępnił pełne API, które można wykorzystać. Kod źródłowy znajduje się pod <a href="https://github.com/KawKac/WoTReservClan" class="text-primary">linkiem</a><br>
+              <div class="small-info">Wykorzystane języki: Kotlin, Groovy</div>
+            </p>
+            <li><h5>Aplikacja do nauki Kotlina</h5></li>
+            <p>
+              Projekt, gdzie pod okiem seniora, uczę się programować w języku Kotlin oraz Groovy. Kod źródłowy można przejrzeć pod <a href="https://github.com/lipov1991/ProductInventory" class="text-primary">linkiem</a>. Aplikacja jest wciąż aktualizowana, ze względów rozwijania swoich umiejętności programistycznych.<br>
+              <div class="small-info">Wykorzystane języki: Kotlin, Groovy</div>
+            </p>
+            <li><h5>Ta strona</h5></li>
+            <p>
+              Głównym moim projektem którym się zajmujem jest ta strona aby ją jak najszybciej ukończyć i później się wziąć za ulepszanie jej (aby zobaczyć kod źródłowy wystarczy kliknąć w ten <a href="https://github.com/KawKac/KawKac.git" class="text-primary">link</a>)<br>
+              <div class="small-info">Wykorzystane języki: HTML, CSS, Bootstrap</div>
+            </p>
+            <li><h5>Gra w kości</h5></li>
+            <p>
+              Prosty program, który polega na wylosowaniu cyfry od 1-6. Aby wygrać należy odgadnąć cyfrę. Co zgadywanie losowana jest nowa cyfra. Program zawiera zarówno grę solo jak i również multiplayer. Do sprawdzania działania programu wykorzystano bota (PC vs. PC)(aby zobaczyć kod źródłowy wystarczy kliknąć w ten <a href="https://github.com/KawKac/Dice" class="text-primary">link</a>).<br>
+              <div class="small-info">Wykorzystane języki: Java</div>
+            </p>
+            <li><h5>Strona na obronę inżyniera</h5></li>
+            <p>
+              Jest to moja praca technika, którą broniłem pod koniec mojego technikum. Jest na temat nowych znaczników HTML, które pojawiły się w HTML5.(aby zobaczyć kod źródłowy wystarczy kliknąć w ten <a href="https://github.com/KawKac/praca_technika" class="text-primary">link</a>).<br>
+              <div class="small-info">Wykorzystane języki: HTML, CSS, JavaScript z biblioteką JQuery</div>
+            </p>
+          </ul>
+        </div>
+      </div>
+      <footer id="footer">
+        <div class="contact_left">
+          <form class="contact_form" action="index.html" method="post">
+            <h1>Formularz kontaktowy</h1><br>
+            <input type="text" name="name" value="" placeholder="Imię i Nazwisko lub nazwa firmy"><br>
+            <input type="text" name="contact" value="" placeholder="Numer telefonu lub e-mail do kontaktu"><br>
+            <textarea name="message" rows="8" cols="80" placeholder="Wiadomość"></textarea><br>
+            <input type="submit" name="submit" value="Wyślij" class="btn btn-success" disabled>
+          </form>
+        </div>
+        <div class="contact_right">
+          <p>Kacper Kawala</p>
+          <p>Adres e-mail: <a href="mailto: kacper.jerzy.kawala@gmail.com" class="text-info">kacper.jerzy.kawala@gmail.com</a></p>
+          <p>Numer telefonu: +48 575-463-884</p>
+        </div>
+      </footer>
